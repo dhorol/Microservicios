cfd1af3e6a51f72cd9e3d9e54c07ae35bb804c63
Jorge Morales <jorge.morales.r@usach.cl>
2023-11-01 17:50:50 -0300

    Inicio de servicios

---
+++ estudiante-service/src/test/java/com/microservicio/estudianteservice/EstudianteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.estudianteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudianteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
