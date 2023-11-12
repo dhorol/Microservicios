bd085c05cb5cb9a468162fa3475ea2223a232445
Jorge Morales <jorge.morales.r@usach.cl>
2023-11-12 02:43:38 -0300

    Cambios para el funcionamiento Eureka-Server

--- cuota-service/src/main/java/Microservicios/example/cuotaservice/CuotaServiceApplication.java
+++ cuota-service/src/main/java/Microservicios/example/cuotaservice/CuotaServiceApplication.java
@@ -2,8 +2,10 @@ package Microservicios.example.cuotaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CuotaServiceApplication {
 
 	public static void main(String[] args) {
--- mensualidad-service/src/main/java/Microservicios/example/mensualidadservice/MensualidadServiceApplication.java
+++ mensualidad-service/src/main/java/Microservicios/example/mensualidadservice/MensualidadServiceApplication.java
@@ -2,8 +2,10 @@ package Microservicios.example.mensualidadservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MensualidadServiceApplication {
 
 	public static void main(String[] args) {
